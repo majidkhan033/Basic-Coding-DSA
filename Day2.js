7df5b5ea6f2caae79da765efbc3c631c597376d4
majidkhan033 <kmajid748@gmail.com>
2022-12-22 21:45:14 +0530

    Day2 of Basic Coding

---
+++ Day2.js
@@ -0,0 +1,42 @@
+// Objects 
+// let person = {
+//     Age : '12',
+//     Name : "JavaScript",
+//     LastName : "Language",
+
+//     phone_number : {
+//         mobile : "1234567890",
+//         landline : "23455"
+//     },
+
+//     callingMethod : function() {
+//         return (`The name of the person is ${person.Name}`);
+//     } 
+// }
+
+// console.log(person.callingMethod());
+// console.log(person.phone_number.mobile);
+
+
+
+
+// --> Method overloading
+
+// function person(age, name, lastname){
+//     this.age = age;
+//     this.name = name;
+//     this.lastname = lastname;
+// }
+
+// function person(lastname){
+//     this.lastname = lastname;
+// }
+
+// let ob1 = new person(21, "abdul", "shaik");
+// let ob2 = new person(21, "majid", "khan");
+
+// let ob3 = new person("abdul");
+
+// console.log(ob2.name);
+// console.log(ob3.lastname);
+
